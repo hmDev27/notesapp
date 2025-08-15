9dddadbe0090d55eb3a0545ba6fd3dec3c199ec9
hmDev27 <hmDev27@gmail.com>
2025-08-15 10:25:44 +0630

    Update App.jsx

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     <div>
       {!backendAvailable && (
       <div className="bg-yellow-100 text-yellow-800 p-4 m-4 rounded text-center border border-yellow-400">
-          ⚠️ Backend Server ကျနေသည်။ Websiteဆက်လက်အသုံးပြုနိုင်ရင် Htet-Min-Oo 09965507737 သို့ဆက်သွယ်ပါ။ 😗
+          ⚠️ Backend Server ကျနေသည်။ Websiteဆက်လက်အသုံးပြုနိုင်ရန် Htet-Min-Oo 0996*****37 သို့ဆက်သွယ်ပါ။ 😗
         </div>
       )}
 
